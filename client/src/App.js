f8b1c3898d5e57df1485c7bbd6d0f9db4188eb3d
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 18:25:52 -0400

    adding navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Content soon to be added </h1>
+        <div className="Navbar">
+          <nav className="navigation-bar">
+            <ul>
+              <li className="nav-item">
+                <a href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">News</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">About</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="landing">
+          <h1>Landing goes here </h1>
+        </div>
+        <div className="footer">
+          <h1> Footer goes here </h1>
+        </div>
       </div>
     );
   }
