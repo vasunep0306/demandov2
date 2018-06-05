56568b03d4c8c2c17b88cc407f94fd7832422e96
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 18:49:23 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Navbar">
-          <ul>
-            <li>
-              <a className="active" href="#home">
-                Demando
-              </a>
-            </li>
-            <li>
-              <a href="#contact">Log In</a>
-            </li>
-            <li>
-              <a href="#about">Sign Up</a>
-            </li>
-          </ul>
-        </div>
+        <Navbar />
         <div className="landing">
           <h1>Landing goes here </h1>
         </div>
