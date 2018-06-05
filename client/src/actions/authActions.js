60e2a986f32054fac9b037a33f706ec3d6d918ab
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 19:54:18 -0400

    outlining

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,3 +1,17 @@
 import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+
+// Register User
+
+// Login - Get User Token
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+// Set logged in user
