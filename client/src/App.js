e2cc45fcfc46f4a61a31ad77dea2f1f771b3c247
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 18:54:21 -0400

    completed footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-
+import Footer from "./components/layout/Footer";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
         <Landing />
-        <div className="footer">
-          <h1> Footer goes here </h1>
-        </div>
+        <Footer />
       </div>
     );
   }
