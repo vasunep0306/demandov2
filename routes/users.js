5b858dbce656984c222f98161c3d55d61d68b965
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 10:40:59 -0400

    adding validation early

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
+
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
