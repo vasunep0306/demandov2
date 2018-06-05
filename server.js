fbf7bcb241837f5a8fcdf5f810c5efcfd7b710d0
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 10:16:54 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
