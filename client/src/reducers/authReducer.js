f1f7235e9aa46547bf073fa7bbdd81ce137570c9
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 20:21:56 -0400

    pushing

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,9 @@
 import isEmpty from "../validation/is-empty";
 
 import { SET_CURRENT_USER } from "../actions/types";
+
+// Set initial state
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
