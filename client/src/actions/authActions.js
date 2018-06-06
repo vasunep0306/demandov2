9bd06197524899f0f64baa99834fdeeceea72a2a
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-05 20:15:36 -0400

    almost done with auth actioon

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -7,11 +7,33 @@ import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 // Register User
 
 // Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to ls
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
 export const setCurrentUser = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
   };
 };
-
-// Set logged in user
