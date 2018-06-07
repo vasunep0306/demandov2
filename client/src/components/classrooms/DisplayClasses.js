8438b2befab797b19684a9984051b145b135b521
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-06-07 19:52:58 -0400

    finished setting up classrooms

--- client/src/components/classrooms/DisplayClasses.js
+++ client/src/components/classrooms/DisplayClasses.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { showClassrooms } from "../../actions/classroomActions";
 
@@ -12,23 +12,29 @@ class DisplayClasses extends Component {
   render() {
     const { classrooms, loading } = this.props.classrooms;
     let classroomArea;
+    const cardStyle = {
+      width: "18rem"
+    };
     if (classrooms === null || loading) {
       classroomArea = <h1>Loading</h1>;
     } else {
-      classroomArea = (
-        <div>
-          <h1>Here are your classes </h1>
-          <table>
-            {classrooms.map(classroom => (
-              <tr key={classroom._id}>
-                <td>{classroom.classcode}</td>
-                <td>{classroom.crn}</td>
-                <td>{classroom.classtitle}</td>
-              </tr>
-            ))}
-          </table>
+      classroomArea = classrooms.map(classroom => (
+        <div className="card" style={{ cardStyle }}>
+          <div className="card-body">
+            <h5 className="card-title">{classroom.classtitle}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              crn:{classroom.crn}
+            </h6>
+            <p className="card-text">Class Code {classroom.classcode}</p>
+            <a href="#" className="card-link">
+              Add Questions
+            </a>
+            <a href="#" className="card-link">
+              Edit Class
+            </a>
+          </div>
         </div>
-      );
+      ));
     }
     return <div>{classroomArea}</div>;
   }
