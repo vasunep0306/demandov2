f7146fbd3d3418e23f3629aced3d74a62b5b1aad
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-06-07 20:07:52 -0400

    made the landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,7 +4,20 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <h1>Landing goes here </h1>
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Demando</h1>
+                <p className="lead">
+                  {" "}
+                  Create a developer profile/portfolio, share posts and get help
+                  from other developers
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
