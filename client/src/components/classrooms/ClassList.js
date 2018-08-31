cf262387851c5472c7cdd620afc149e91a27ef37
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-08-31 12:31:53 -0400

    create classroom file

---
+++ client/src/components/classrooms/ClassList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+class ClassList extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ClassList;
