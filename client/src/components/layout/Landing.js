e9ea319687a17d70840a14a829543a2eb6b40f82
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-09-14 17:12:32 -0400

    working on changing the landing text

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -9,11 +9,7 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4 title">Demando</h1>
-                <p className="lead">
-                  {" "}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
-                </p>
+                <p className="lead"> Demand Students To Participate</p>
               </div>
             </div>
           </div>
