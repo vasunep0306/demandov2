3ab8da5390198f6a8c469aefa664a2aa82c327c7
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-09-16 09:30:05 -0400

    create table and call function

--- client/src/components/classrooms/ClassList.js
+++ client/src/components/classrooms/ClassList.js
@@ -2,11 +2,54 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import { getStudents } from "../../actions/classroomActions";
 
 class ClassList extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1>Classlist</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Student Name</th>
+              <th scope="col">Student Email</th>
+              <th scope="col">Teacher Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
+ClassList.propTypes = {
+  classrooms: PropTypes.object.isRequired,
+  getStudents: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
 
-export default ClassList;
+export default connect(
+  mapStateToProps,
+  { getStudents }
+)(withRouter(ClassList));
