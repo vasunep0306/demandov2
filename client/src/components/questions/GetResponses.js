05d4e828891792891210061d393c217f18f66f82
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-10-07 08:15:46 -0400

    starting to implement answers page

---
+++ client/src/components/questions/GetResponses.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { withRouter, Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+class GetResponses extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+//TODO: Insert PropTypes here
+
+//TODO: Insert mapStateToProps here
+
+export default GetResponses;
