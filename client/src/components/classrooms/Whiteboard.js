bb925c657a522f2a1cdb11e2834911f125c7dcef
Vasanth Neppalli <vasunep0306@gmail.com>
2018-11-10 13:05:37 -0500

    backspace test

--- client/src/components/classrooms/Whiteboard.js
+++ client/src/components/classrooms/Whiteboard.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 //https://codepen.io/HarryGateaux/pen/BApxl
-export default class Whiteboard extends Component {
+class Whiteboard extends Component {
   render() {
     return <div />;
   }
 }
+
+export default Whiteboard;
