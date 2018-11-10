deadea10449b2215ecc7c919907209c04fc231fb
Vasanth Neppalli <vasunep0306@gmail.com>
2018-11-10 12:44:04 -0500

    started to create the whiteboard component

---
+++ client/src/components/classrooms/Whiteboard.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+//https://codepen.io/HarryGateaux/pen/BApxl
+export default class Whiteboard extends Component {
+  render() {
+    return <div />;
+  }
+}
