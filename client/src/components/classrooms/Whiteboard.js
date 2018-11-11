7297e20c2a712b78b24403a2b952912c9a2be6fc
Vasanth Neppalli <vasunep0306@gmail.com>
2018-11-11 11:10:01 -0500

    got canvas component to work

--- client/src/components/classrooms/Whiteboard.js
+++ client/src/components/classrooms/Whiteboard.js
@@ -11,16 +11,45 @@ class Whiteboard extends Component {
       brushColor: "#800909",
       lineWidth: 4,
       canvasStyle: {
-        backgroundColor: "#00FFDC"
+        backgroundColor: "#000000"
       },
       clear: false
     };
   }
+
+  handleOnClickClear() {
+    this.setState({
+      clear: true
+    });
+  }
+  handleOnClickChangeColorYellow() {
+    this.setState({
+      brushColor: "#ffff00",
+      clear: false
+    });
+  }
+
+  handleOnClickChangeColorRed() {
+    this.setState({
+      brushColor: "#800909",
+      clear: false
+    });
+  }
+
   render() {
     return (
       <div>
         <div>
-          <DrawableCanvas />
+          <DrawableCanvas {...this.state} />
+          <button onClick={this.handleOnClickClear.bind(this)}>
+            Clear all
+          </button>
+          <button onClick={this.handleOnClickChangeColorYellow.bind(this)}>
+            Set color to Yellow
+          </button>
+          <button onClick={this.handleOnClickChangeColorRed.bind(this)}>
+            Set color to Red
+          </button>
         </div>
       </div>
     );
