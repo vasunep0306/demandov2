3f4715ac252ddea15bfcb493c975ba1ec9b52ad8
Vasanth Neppalli <vasunep0306@gmail.com>
2018-11-11 09:50:46 -0500

    populate render method

--- client/src/components/common/Canvas.js
+++ client/src/components/common/Canvas.js
@@ -1,3 +1,17 @@
 import React, { Component } from "react";
 
-class Canvas extends Component {}
+class Canvas extends Component {
+  render() {
+    return (
+      <canvas
+        // We use the ref attribute to get direct access to the canvas element.
+        ref={ref => (this.canvas = ref)}
+        style={{ background: "black" }}
+        onMouseDown={this.onMouseDown}
+        onMouseLeave={this.endPaintEvent}
+        onMouseUp={this.endPaintEvent}
+        onMouseMove={this.onMouseMove}
+      />
+    );
+  }
+}
