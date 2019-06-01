bf7f1709bc3a6c115da633138dfd0f15c90fa977
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-01 15:58:53 -0400

    Change get to post on login

--- routes/api/users.js
+++ routes/api/users.js
@@ -52,7 +52,7 @@ router.post("/register", (req, res) => {
   });
 });
 
-/** @route   GET api/users/login
+/** @route   POST api/users/login
  * @desc    Login User / Returning JWT Token
  * @access  Public
  */
