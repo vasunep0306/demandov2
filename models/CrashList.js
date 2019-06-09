32f5384fca07811db3e92ba1d92e82ed46d15d66
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-09 10:59:22 -0400

    add date to crashlist

--- models/CrashList.js
+++ models/CrashList.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CrashSchema = new Schema({});
+const CrashSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 module.exports = CrashList = mongoose.model("crashlists", CrashSchema);
