347e91a0a3f5b71975a28dc99ac177b4d39a5aad
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-15 11:44:13 -0400

    update discussion.js

--- validation/discussion.js
+++ validation/discussion.js
@@ -3,6 +3,8 @@ const isEmpty = require("./isEmpty");
 
 module.exports = function validateDiscussionInput(data) {
   let errors = {};
+  // The discussion topic is mandatory
+  // The discussion body is mandatory
 
   return {
     errors,
