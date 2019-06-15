f675835063c9069240c1a094c8e2d7cdae88dc03
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-15 11:43:12 -0400

    set up skeleton

--- validation/discussion.js
+++ validation/discussion.js
@@ -1,4 +1,11 @@
 const Validator = require("validator");
 const isEmpty = require("./isEmpty");
 
-module.exports = function validateDiscussionInput(data) {};
+module.exports = function validateDiscussionInput(data) {
+  let errors = {};
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
