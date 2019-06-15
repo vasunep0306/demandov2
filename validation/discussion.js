cfdc9f740dba367b24ca620a327e93d480cd46f9
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-15 12:04:18 -0400

    fill in sections

--- validation/discussion.js
+++ validation/discussion.js
@@ -4,9 +4,17 @@ const isEmpty = require("./isEmpty");
 module.exports = function validateDiscussionInput(data) {
   let errors = {};
   // The discussion topic is mandatory
-  data.topic = !isEmpty(data.topic) ? data.topic : "";
+  data.discussionTopic = !isEmpty(data.discussionTopic)
+    ? data.discussionTopic
+    : "";
   // The discussion subject is mandatory
+  data.discussionSubject = !isEmpty(data.discussionSubject)
+    ? data.discussionSubject
+    : "";
   // The discussion body is mandatory
+  data.discussionBody = !isEmpty(data.discussionBody)
+    ? data.discussionBody
+    : "";
 
   return {
     errors,
