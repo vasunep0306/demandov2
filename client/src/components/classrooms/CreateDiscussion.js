38fa3752dfe01bf3ba832dfc5e750d6666c57f45
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-16 17:41:14 -0400

    add import statements

---
+++ client/src/components/classrooms/CreateDiscussion.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getClass } from "../../actions/classroomActions";
