655c5f02064a9f23fa0762ad6ae390321aa0403b
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-16 12:22:31 -0400

    add getClass

--- client/src/components/classrooms/DiscussionList.js
+++ client/src/components/classrooms/DiscussionList.js
@@ -3,4 +3,11 @@ import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { getAuthor } from "../../actions/authActions";
-import { getDiscussions } from "../../actions/classroomActions";
+import { getDiscussions, getClass } from "../../actions/classroomActions";
+
+class DiscussionList extends Component {}
+
+export default connect(
+  mapStateToProps,
+  { getAuthor, getDiscussions, getClass }
+)(withRouter(DiscussionList));
