a6a87157455a8216016be4f2c1471cb2e181ce77
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-16 17:40:19 -0400

    remove get author function

--- client/src/components/classrooms/DiscussionList.js
+++ client/src/components/classrooms/DiscussionList.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter, Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { getAuthor } from "../../actions/authActions";
 import { getDiscussions, getClass } from "../../actions/classroomActions";
 
 class DiscussionList extends Component {
