5db5f2088e072b61219f149e8b2272141eb5119a
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-16 13:06:09 -0400

    finish DiscussionList.js

--- client/src/components/classrooms/DiscussionList.js
+++ client/src/components/classrooms/DiscussionList.js
@@ -13,15 +13,50 @@ class DiscussionList extends Component {
 
   render() {
     let discussionList;
+    let headerTitle;
     let { discussions, classroom, loading } = this.props.classrooms;
     if (classroom === null || loading) {
       discussionList = <h1>Loading</h1>;
+      headerTitle = "Page is still loading";
     } else if (discussions === null || loading) {
       discussionList = <h1>Loading</h1>;
-    } else if (discussions.nodiscussion) {
-      listOfStudents = <h1>{discussions.nodiscussion}</h1>;
+      headerTitle = "Page is still loading";
+    } else if (discussions.nodiscussions) {
+      headerTitle = `Discussions for ${classroom.classtitle}`;
+      discussionList = <h1>{discussions.nodiscussions}</h1>;
     } else {
+      headerTitle = `Discussions for ${classroom.classtitle}`;
+      discussions = Array.from(this.props.classrooms.discussions);
+      discussionList = discussions.map(discussion => {
+        <div>
+          <tr>
+            <td>{getAuthor(discussion.author)}</td>
+            <td>{discussion.discussionTopic}</td>
+            <td>
+              <a href="#">Go To Discussion</a>
+            </td>
+            <td>{discussion.date}</td>
+          </tr>
+        </div>;
+      });
     }
+
+    return (
+      <div className="container">
+        <h2>{headerTitle}</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Author Name</th>
+              <th>Discussion Topic</th>
+              <th>Link</th>
+              <th>Date Posted</th>
+            </tr>
+          </thead>
+          <tbody>{discussionList}</tbody>
+        </table>
+      </div>
+    );
   }
 }
 
