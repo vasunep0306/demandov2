e4d04ba86e6f55fafdc1a461a33bfca987ccb206
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2019-06-17 18:07:44 -0400

    stage

--- client/src/components/classrooms/DiscussionPage.js
+++ client/src/components/classrooms/DiscussionPage.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { getComments, addComment } from "../../actions/classroomActions";
 
-export default class DiscussionPage extends Component {
+class DiscussionPage extends Component {
   render() {
     return (
       <div>
@@ -9,3 +13,19 @@ export default class DiscussionPage extends Component {
     );
   }
 }
+
+DiscussionPage.propTypes = {
+  classroom: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  getComments: PropTypes.func.isRequired,
+  addComment: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { getComments, addComment }
+)(withRouter(DiscussionPage));
